944ea46d58ea8defb49477209adc26372e939148
Dario <dhtarnowski75@gmail.com>
2022-02-25 23:14:54 -0300

    lista de items

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { ButtomComponent } from './components/buttom/buttom.component';
+import { TaskComponent } from './components/task/task.component';
+import { TaskItemComponent } from './components/task-item/task-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ButtomComponent
+    ButtomComponent,
+    TaskComponent,
+    TaskItemComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/task/task.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { Task } from 'src/app/tasks';
+import { TASKS } from 'src/app/mock-tasks';
+
+@Component({
+  selector: 'app-task',
+  templateUrl: './task.component.html',
+  styleUrls: ['./task.component.css']
+})
+export class TaskComponent implements OnInit {
+  tasks: Task[] = TASKS;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
